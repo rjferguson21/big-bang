b95e2712067f09ab8060e1217cc92130aee7bc1c
Kevin Wilder <kevin.wilder@darkwolfsolutions.com>
2021-04-23 13:46:29 -0600

    increase hr wait time

--- scripts/deploy/02_wait_for_helmreleases.sh
+++ scripts/deploy/02_wait_for_helmreleases.sh
@@ -32,7 +32,7 @@ function array_contains() {
 ## $1: package name
 function wait_on() {
   echo "Waiting on package $1"
-  kubectl wait --for=condition=Ready --timeout 600s helmrelease -n bigbang $1;
+  kubectl wait --for=condition=Ready --timeout 1200s helmrelease -n bigbang $1;
 }
 
 ## Function to wait on all statefulsets
