f807fcd01e79519de2c9bb72febefab36e648049
branden.cobb <cobb_branden@bah.com>
2021-04-23 18:59:35 +0000

    Update 04_linting.sh

--- tests/bash/04_linting.sh
+++ tests/bash/04_linting.sh
@@ -11,7 +11,13 @@ clusterlint run
 echo "======================================"
 echo "Istioctl Analyze"
 echo "======================================"
-istioctl analyze --all-namespaces --suppress "IST0102"
+istioctl analyze --all-namespaces \
+  --suppress "IST0101=VirtualService *" \
+  --suppress "IST0104=Gateway *" \
+  --suppress "IST0105=Pod *" \
+  --suppress "IST0102=Namespace *" \
+  --suppress "IST0118=Service *" \
+  --suppress "IST0107=Deployment *"
 echo "======================================"
 echo "Popeye"
 echo "======================================"
