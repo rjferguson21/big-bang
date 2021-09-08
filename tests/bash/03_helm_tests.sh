d43522e0ccb1b749d3dadcf381b4362341c8d4bb
Micah Nagel <micah.nagel@parsons.com>
2021-09-08 11:41:13 -0600

    log better

--- tests/bash/03_helm_tests.sh
+++ tests/bash/03_helm_tests.sh
@@ -12,11 +12,11 @@ if [[ $(tail -c 1 newhosts) != "" ]]; then
 fi
 
 # Get each VS hostname + ingress gateway IP and add to newhosts
-for vs in $(kubectl get vs -A -o go-template='{{range .items}}{{.metadata.name}}{{":"}}{{.metadata.namespace}}{{" "}}{{end}}'); do
+for vs in $(kubectl get virtualservice -A -o go-template='{{range .items}}{{.metadata.name}}{{":"}}{{.metadata.namespace}}{{" "}}{{end}}'); do
   vs_name=$(echo ${vs} | awk -F: '{print $1}')
   vs_namespace=$(echo ${vs} | awk -F: '{print $2}')
-  hosts=$(kubectl get vs ${vs_name} -n ${vs_namespace} -o go-template='{{range .spec.hosts}}{{.}}{{" "}}{{end}}')
-  gateway=$(kubectl get vs ${vs_name} -n ${vs_namespace} -o jsonpath='{.spec.gateways[0]}' | awk -F/ '{print $2}')
+  hosts=$(kubectl get virtualservice ${vs_name} -n ${vs_namespace} -o go-template='{{range .spec.hosts}}{{.}}{{" "}}{{end}}')
+  gateway=$(kubectl get virtualservice ${vs_name} -n ${vs_namespace} -o jsonpath='{.spec.gateways[0]}' | awk -F/ '{print $2}')
   ingress_gateway=$(kubectl get gateway -n istio-system $gateway -o jsonpath='{.spec.selector.app}')
   external_ip=$(kubectl get svc -n istio-system $ingress_gateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
   for host in $hosts; do
@@ -46,19 +46,24 @@ for hr in $installed_helmreleases; do
   echo "$test_result"
   namespace=$(echo "$test_result" | yq eval '."NAMESPACE"' -)
   test_suite=$(echo "$test_result" | yq eval '.["TEST SUITE"]' -)
-
   if [ ! $test_suite == "None" ]; then
-    mkdir -p cypress-tests/${hr}/tests/cypress
-    echo "***** Helm Test Logs for ${hr} *****"
-    kubectl logs --tail=-1 -n ${namespace} -l helm-test=enabled
-    echo "***** End Helm Test Logs for ${hr} *****"
+    echo "***** Helm Test Logs for Helmrelease ${hr} *****"
+    for pod in $(cat output | grep "TEST SUITE" | grep "test" | awk -F: '{print $2}' | xargs); do
+      if kubectl get pod -n ${namespace} ${pod} &>/dev/null; then
+        echo -e "---\nLogs for ${pod}:\n---"
+        kubectl logs --tail=-1 -n ${namespace} ${pod}
+      fi
+    done
+    echo "***** End Helm Test Logs for Helmrelease ${hr} *****"
     if kubectl get configmap -n ${namespace} cypress-screenshots &>/dev/null; then
+      mkdir -p cypress-tests/${hr}/tests/cypress
       kubectl get configmap -n ${namespace} cypress-screenshots -o jsonpath='{.data.cypress-screenshots\.tar\.gz\.b64}' > cypress-screenshots.tar.gz.b64
       cat cypress-screenshots.tar.gz.b64 | base64 -d > cypress-screenshots.tar.gz
       tar -zxf cypress-screenshots.tar.gz --strip-components=2 -C cypress-tests/${hr}/tests/cypress
       rm -rf cypress-screenshots.tar.gz.b64 cypress-screenshots.tar.gz
     fi
     if kubectl get configmap -n ${namespace} cypress-videos &>/dev/null; then
+      mkdir -p cypress-tests/${hr}/tests/cypress
       kubectl get configmap -n ${namespace} cypress-videos -o jsonpath='{.data.cypress-videos\.tar\.gz\.b64}' > cypress-videos.tar.gz.b64
       cat cypress-videos.tar.gz.b64 | base64 -d > cypress-videos.tar.gz
       tar -zxf cypress-videos.tar.gz --strip-components=2 -C cypress-tests/${hr}/tests/cypress
@@ -69,8 +74,6 @@ for hr in $installed_helmreleases; do
     fi
   fi
 done
-
-sleep 600
 
 if [ $ERRORS -gt 0 ]; then
   echo "Encountered $ERRORS errors while running tests. See output logs above and artifacts in the job."
