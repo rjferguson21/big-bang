41f02ab97d567ae5270dbf3f7e5553bd9879b804
Mark Sanchez <mark.sanchez.8@us.af.mil>
2021-09-15 12:30:39 -0500

    add disable_servicelb_config.yaml

--- tests/ci/k3d/deploy_k3d.sh
+++ tests/ci/k3d/deploy_k3d.sh
@@ -2,4 +2,9 @@
 
 set -ex
 
-k3d cluster create ${CI_JOB_ID} --config tests/ci/k3d/config.yaml --network ${CI_JOB_ID}
+if [[ $CI_MERGE_REQUEST_LABELS =~ "keycloak"]] || [[ $CI_MERGE_REQUEST_LABELS =~ "all-packages" ]]; then
+  echo "keycloak is present"
+  k3d cluster create ${CI_JOB_ID} --config tests/ci/k3d/disable_servicelb_config.yaml --network ${CI_JOB_ID}
+else
+  k3d cluster create ${CI_JOB_ID} --config tests/ci/k3d/config.yaml --network ${CI_JOB_ID}
+fi
