851dc878792ba12dc4060136d1610585bd1ad8ed
Mark Sanchez <mark.sanchez.8@us.af.mil>
2021-09-15 13:12:43 -0500

    testing deploy_k3d.sh

--- tests/ci/k3d/deploy_k3d.sh
+++ tests/ci/k3d/deploy_k3d.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 set -ex
-
-if [[ $CI_MERGE_REQUEST_LABELS =~ "keycloak" ||  $CI_MERGE_REQUEST_LABELS =~ "all-packages" ]]; then
+echo "$CI_MERGE_REQUEST_LABELS"
+echo $CI_MERGE_REQUEST_LABELS
+if [[ "$CI_MERGE_REQUEST_LABELS" =~ "keycloak" ||  "$CI_MERGE_REQUEST_LABELS" =~ "all-packages" ]]; then
   echo "keycloak is present"
   k3d cluster create ${CI_JOB_ID} --config tests/ci/k3d/disable_servicelb_config.yaml --network ${CI_JOB_ID}
 else
