137ef873fc5a5b6e382484567dd453bb6b4db423
Mark Sanchez <mark.sanchez.8@us.af.mil>
2021-09-15 19:10:03 -0500

    testing public ingress with metallb

--- tests/ci/k3d/deploy_k3d.sh
+++ tests/ci/k3d/deploy_k3d.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-if [[ $CI_MERGE_REQUEST_LABELS =~ "keycloak" ||  $CI_MERGE_REQUEST_LABELS =~ "all-packages" ]]; then
+# if [[ $CI_MERGE_REQUEST_LABELS =~ "keycloak" ||  $CI_MERGE_REQUEST_LABELS =~ "all-packages" ]]; then
   k3d cluster create ${CI_JOB_ID} --config tests/ci/k3d/disable-servicelb-config.yaml --network ${CI_JOB_ID}
-else
-  k3d cluster create ${CI_JOB_ID} --config tests/ci/k3d/config.yaml --network ${CI_JOB_ID}
-fi
+# else
+#   k3d cluster create ${CI_JOB_ID} --config tests/ci/k3d/config.yaml --network ${CI_JOB_ID}
+# fi
