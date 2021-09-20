b0f1ca0741bec4f86aaac58f3eeedad30d0fb322
Mark Sanchez <mark.sanchez.8@us.af.mil>
2021-09-20 14:15:51 -0500

    fix styling

--- tests/tests/01_virtualservices.sh
+++ tests/tests/01_virtualservices.sh
@@ -10,11 +10,10 @@ echo "Checking "
 
 hosts=`kubectl get virtualservices -A -o jsonpath="{ .items[*].spec.hosts[*] }"`
 for host in $hosts; do
-    if [ $host == "keycloak.bigbang.dev" ]; then
+  if [ $host == "keycloak.bigbang.dev" ]; then
     echo "$ip_passthrough $host" >> /etc/hosts
-    curl -svv https://$host/ > /dev/null
-    else
+  else
     echo "$ip $host" >> /etc/hosts
-    curl -svv https://$host/ > /dev/null
-    fi
+  fi
+  curl -svv https://$host/ > /dev/null
 done
