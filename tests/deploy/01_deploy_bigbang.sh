57bd9dd860340a4e8fd01114576271e23a770fcb
Mark Sanchez <mark.sanchez.8@us.af.mil>
2021-09-20 14:20:33 -0500

    styling

--- tests/deploy/01_deploy_bigbang.sh
+++ tests/deploy/01_deploy_bigbang.sh
@@ -40,16 +40,16 @@ helm upgrade -i bigbang chart -n bigbang --create-namespace \
   --set registryCredentials[0].registry=registry1.dso.mil \
   -f ${CI_VALUES_FILE}
 
-  # apply secrets kustomization pointing to current branch or master if an upgrade job
-  if [[ $(git branch --show-current) == "${CI_DEFAULT_BRANCH}" ]]; then
-    echo "Deploying secrets from the ${CI_DEFAULT_BRANCH} branch"
-    kubectl apply -f tests/ci/shared-secrets.yaml
-  elif [ -z "$CI_COMMIT_TAG" ]; then
-    echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} branch"
-    cat tests/ci/shared-secrets.yaml | sed 's|master|'"$CI_COMMIT_REF_NAME"'|g' | kubectl apply -f -
-  else
-    echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} tag"
-    # NOTE: $CI_COMMIT_REF_NAME = $CI_COMMIT_TAG when running on a tagged build
-    cat tests/ci/shared-secrets.yaml | sed 's|branch: master|tag: '"$CI_COMMIT_REF_NAME"'|g' | kubectl apply -f -
-  fi
+# apply secrets kustomization pointing to current branch or master if an upgrade job
+if [[ $(git branch --show-current) == "${CI_DEFAULT_BRANCH}" ]]; then
+  echo "Deploying secrets from the ${CI_DEFAULT_BRANCH} branch"
+  kubectl apply -f tests/ci/shared-secrets.yaml
+elif [ -z "$CI_COMMIT_TAG" ]; then
+  echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} branch"
+  cat tests/ci/shared-secrets.yaml | sed 's|master|'"$CI_COMMIT_REF_NAME"'|g' | kubectl apply -f -
+else
+  echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} tag"
+  # NOTE: $CI_COMMIT_REF_NAME = $CI_COMMIT_TAG when running on a tagged build
+  cat tests/ci/shared-secrets.yaml | sed 's|branch: master|tag: '"$CI_COMMIT_REF_NAME"'|g' | kubectl apply -f -
+fi
 
