55c0c279d531d9789f5e9b7a81718d0d62d79e15
Branden Cobb <cobb_branden@bah.com>
2021-09-22 13:42:23 -0600

    order doesn't matter

--- tests/deploy/01_deploy_bigbang.sh
+++ tests/deploy/01_deploy_bigbang.sh
@@ -39,12 +39,12 @@ helm upgrade -i bigbang chart -n bigbang --create-namespace \
   -f ${CI_VALUES_FILE}
 
 # apply secrets kustomization pointing to current branch or master if an upgrade job
-if [[ $(git branch --show-current) == "${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}" ]]; then
-  echo "Deploying secrets from the ${CI_MERGE_REQUEST_TARGET_BRANCH_NAME} branch"
-  cat tests/ci/shared-secrets.yaml | sed 's|master|'"$CI_MERGE_REQUEST_TARGET_BRANCH_NAME"'|g' | kubectl apply -f -
-elif [[ $(git branch --show-current) == "${CI_DEFAULT_BRANCH}" ]]; then
+if [[ $(git branch --show-current) == "${CI_DEFAULT_BRANCH}" ]]; then
   echo "Deploying secrets from the ${CI_DEFAULT_BRANCH} branch"
   kubectl apply -f tests/ci/shared-secrets.yaml
+elif [[ $(git branch --show-current) == "${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}" ]]; then
+  echo "Deploying secrets from the ${CI_MERGE_REQUEST_TARGET_BRANCH_NAME} branch"
+  cat tests/ci/shared-secrets.yaml | sed 's|master|'"$CI_MERGE_REQUEST_TARGET_BRANCH_NAME"'|g' | kubectl apply -f -
 elif [ -z "$CI_COMMIT_TAG" ]; then
   echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} branch"
   cat tests/ci/shared-secrets.yaml | sed 's|master|'"$CI_COMMIT_REF_NAME"'|g' | kubectl apply -f -
